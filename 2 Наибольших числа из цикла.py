3798d98d100e5bcc8bae81522b5e7eecbe0059e0
PaxaXimik <User@WIN-64PGF5PMLR2.(none)>
2022-12-01 11:31:32 +0300

    Изменено

--- 2 Наибольших числа из цикла.py
+++ 2 Наибольших числа из цикла.py
@@ -1,11 +1,11 @@
-num = int(input())
+num = int(input('Введите количество повторений: '))
 large=0
 largest=0
 for i in range(num): #вводим количество повторений, которое необходимо
-    n=int(input()) #вводим сами числа
+    n=int(input('Введите число: ')) #вводим сами числа
     if n > large: #если, n всегда больше предыдущего, то можно было обойтись и этим решением
         largest=large
         large=n
     elif n>largest:
         largest=n
-print(largest), print (large) 
+print(f'Число меньше наибольшего из введенных чисел {largest}'), print (f'Наибольшее из введенных чисел {large}') 
